38bf19f4fc05bb2b009d59ff4b07021b237c5a40
Hassan <hassan.h.najm@gmail.com>
2024-02-18 19:24:47 +0100

    Création du model et  admin site register

--- password_manager/sites/models.py
+++ password_manager/sites/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Site(models.Model):
+    nom = models.CharField(max_length=25)
+    url = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    identifiant = models.CharField(max_length=50, null=True)
+    password = models.CharField(max_length=16)
